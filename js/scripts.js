e2261aed7c93c4cf0d31d889f75ce899f98f936a
Katherine Parker <kparker.adrienne@gmail.com>
2021-03-18 15:28:06 -0400

    js fix for search bar

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,7 @@ $(function() {
         $(this).toggleClass('open');
     });
 });
+
+$('.searchForm input').click(function(e) {
+    e.stopPropagation(); //This will prevent the event from bubbling up and close the dropdown when you type/click on text boxes.
+});
